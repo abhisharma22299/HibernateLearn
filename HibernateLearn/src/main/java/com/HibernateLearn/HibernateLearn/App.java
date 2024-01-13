d512d04be9927a69f71ba2978c5af0497c164e8c
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-13 11:56:38 +0530

    HibernateLearning

---
+++ HibernateLearn/src/main/java/com/HibernateLearn/HibernateLearn/App.java
@@ -0,0 +1,13 @@
+package com.HibernateLearn.HibernateLearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
