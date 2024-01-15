20818b4a40fd28f7eea6d777bd6f61fed8274b28
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-15 20:12:55 +0530

    Hibernate

--- HibernateLearn/src/main/java/com/HibernateLearn/HibernateLearn/App.java
+++ HibernateLearn/src/main/java/com/HibernateLearn/HibernateLearn/App.java
@@ -1,13 +1,17 @@
 package com.HibernateLearn.HibernateLearn;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Project Started!" );
+        // this helps to get the connection 
+        Configuration config=new Configuration();
+        config.configure("hibernate.cfg.xml");
+      SessionFactory sf	=config.buildSessionFactory();
+   System.out.println(sf);
     }
 }
