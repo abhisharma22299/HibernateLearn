7665e267f7ae3c5025503a2c26371f526bc15db4
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-15 23:51:44 +0530

    Hibernate

--- HibernateLearn/src/main/java/com/HibernateLearn/HibernateLearn/App.java
+++ HibernateLearn/src/main/java/com/HibernateLearn/HibernateLearn/App.java
@@ -1,17 +1,38 @@
 package com.HibernateLearn.HibernateLearn;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Project Started!" );
-        // this helps to get the connection 
-        Configuration config=new Configuration();
-        config.configure("hibernate.cfg.xml");
-      SessionFactory sf	=config.buildSessionFactory();
-   System.out.println(sf);
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Project Started!");
+		// this helps to get the connection
+		Configuration config = new Configuration();
+
+		// specify the path correctly
+//        config.configure("com/tut/hibernate.cfg.xml");
+		config.configure("hibernate.cfg.xml");
+		SessionFactory sf = config.buildSessionFactory();
+		// create Student Object
+		Student s1 = new Student();
+		// set the values of the Student class
+		s1.setName("Abhi");
+		s1.setAge(10);
+		s1.setCity("Lucknow");
+		s1.setRollnumber(2);
+
+		System.out.println(s1);
+
+		Session session = sf.openSession();
+
+		Transaction sc = session.beginTransaction();
+		session.save(s1);
+		// session close
+		//
+		sc.commit();
+
+		session.close();
+	}
 }
---
+++ HibernateLearn/src/main/java/com/HibernateLearn/HibernateLearn/Student.java
@@ -0,0 +1,69 @@
+package com.HibernateLearn.HibernateLearn;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//this is use for the change the entire entity @Entity(name="Student_Details")
+@Entity
+@Table(name="MyStudentData")
+public class Student {
+//	@Genrated(value="")
+	@Id
+	private int rollnumber;
+	private String name;
+	private int Age;
+
+
+	private String city;
+
+	public Student(int rollnumber, String name, int age, String city) {
+		super();
+		this.rollnumber = rollnumber;
+		this.name = name;
+		Age = age;
+		this.city = city;
+
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getRollnumber() {
+		return rollnumber;
+	}
+
+	public void setRollnumber(int rollnumber) {
+		this.rollnumber = rollnumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return Age;
+	}
+
+	public void setAge(int age) {
+		Age = age;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [rollnumber=" + rollnumber + ", name=" + name + ", Age=" + Age + ", city=" + city + "]";
+	}
+}
