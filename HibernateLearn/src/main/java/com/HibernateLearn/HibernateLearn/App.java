40f05613346c7c546a06a12b4a760a6e0d01376b
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-16 00:58:36 +0530

    Hibernate

--- HibernateLearn/src/main/java/com/HibernateLearn/HibernateLearn/App.java
+++ HibernateLearn/src/main/java/com/HibernateLearn/HibernateLearn/App.java
@@ -1,12 +1,15 @@
 package com.HibernateLearn.HibernateLearn;
 
+import java.io.FileInputStream;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("Project Started!");
 		// this helps to get the connection
 		Configuration config = new Configuration();
@@ -21,17 +24,29 @@ public class App {
 		s1.setName("Abhi");
 		s1.setAge(10);
 		s1.setCity("Lucknow");
-		
 		System.out.println(s1);
 
+		// creating the address class object
+		Address ad = new Address();
+		ad.setAddedDate(new Date());
+		ad.setStreet("street");
+		ad.setCity("Lucknow");
+		// Reading image
+		FileInputStream fis =new FileInputStream("src/main/java/1.PNG");
+		byte[] data=new byte[fis.available()];
+		fis.read();
+		ad.setImage(data);
+		ad.setX(12122.21);
 		Session session = sf.openSession();
 
 		Transaction sc = session.beginTransaction();
 		session.save(s1);
+		session.save(ad);
 		// session close
 		//
 		sc.commit();
 
 		session.close();
+		System.out.println("Done....");
 	}
 }
