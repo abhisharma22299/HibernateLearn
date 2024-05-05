c774fdcd40b4e851b948e0007e7df798847438f3
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-05-05 11:12:24 +0530

    Notes

---
+++ hibernate2/hibernateLearn2/src/main/java/hibernateLearn2/hibernateLearn2/App.java
@@ -0,0 +1,13 @@
+package hibernateLearn2.hibernateLearn2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
